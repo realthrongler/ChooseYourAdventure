65886bab752939fde6baa5a3cd61aa8748d9e763
realthrongler <fjack6677@gmail.com>
2025-03-27 14:10:19 -0400

    Update main.py
    Laying the foundation and testing some things.

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("scooby doo? more like sceeby dee")
+#importing modules
+import time
+import random
+import colorama
+from colorama import Fore, Style
+
+print(Fore.GREEN + "Yo it's ya boy Noah here the DJ!" + Style.RESET_ALL)
+
+start_game = input("Type BAM to start the game: ")
+
+while start_game != "BAM":
+    print("You need to type 'BAM' to start the game.")
+    start_game = input("Type 'BAM' to begin the game")
+
+print("test line one")
+time.sleep(3)
+print("test line two")
