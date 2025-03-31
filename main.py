dc12f599be49bd9962c577cd0e4aaf8ab946228a
realthrongler <fjack6677@gmail.com>
2025-03-31 10:13:40 -0400

    Update main.py
    refer to changelog

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ print("2. Disguise yourself as a guard and walk onto the boat")
 
 choice = input("Please enter '1' or '2'.\n")
 if choice == "1":
+
     #Swimming route
     print("You delve into the biting cold of the ocean water, accompanied only by the hiss of your oxygen mask.")
     time.sleep(short_delay)
@@ -54,6 +55,7 @@ if choice == "1":
     print("2. Go downstairs")
     choice = input("Please enter '1' or '2'.\n")
     if choice == "1":
+
         #Upstairs route
         print("You climb the stairs and reach what appears to be a break room.")
         time.sleep(regular_delay)
@@ -73,6 +75,7 @@ if choice == "1":
         time.sleep(regular_delay)
         choice = input('"Do you like my game?" y/n\n')
         if choice == "yes" or "y":
+
             #I love this amazing game route
             print('"Good choice"')
             time.sleep(long_delay)
@@ -110,11 +113,13 @@ if choice == "1":
             print("Congratulations! You have successfully retrieved the devious plans of Mr.Lamb successfully,\nthe contents of which will be revealed in the sequel.\n(sold separately)")
             
         elif choice == "no" or "n":
+
             #I wouldn't know art if it slapped me in the face route
             print('"Oh... alright then."')
             time.sleep(long_delay)
             print("You die of a heart attack.")
     elif choice == "2":
+
         #Downstairs route
         print("You descend the stairs, reaching a room filled with servers and computers and blinking lights (for cinematic effect)")
         time.sleep(long_delay)
@@ -139,6 +144,7 @@ if choice == "1":
         time.sleep(long_delay)
         choice = input("What do you do?\n")
         if choice == "1":
+
             #Confrontation route
             print("You come out of the shadows, ready to strike.")
             time.sleep(regular_delay)
@@ -157,6 +163,7 @@ if choice == "1":
             time.sleep(regular_delay)
             print("You get back to your base of operations successfully. Sifting through many pirated games, you find Mr.Lamb's evil plans,\n the contents of which will be revealed in the sequel\n(sold separately)")
         elif choice == "2":
+
             #Run away route
             print("You find yourself shaking in your boots because of his menacing aura.")
             time.sleep(regular_delay)
@@ -166,6 +173,7 @@ if choice == "1":
             time.sleep(long_delay)
             print("You jump overboard, returning home in shame. Mr.Lamb was too powerful for your mere mortal mind to handle.")
 elif choice == "2":
+
     #guard disguise route
     print("You find a lone guard at the docks and knock them out, stealing their uniform.")
     time.sleep(regular_delay)
@@ -182,6 +190,7 @@ elif choice == "2":
     time.sleep(short_delay)
     choice = input("What do you do?\n")
     if choice == "1":
+
         #flirt route
         print("You remember Mr.Nagra's masterclass on flirting, and conjure up a line sure to seduce this guard.")
         time.sleep(regular_delay)
@@ -195,10 +204,99 @@ elif choice == "2":
         time.sleep(long_delay)
         print(Fore.RED + "you are shot and killed." + Style.RESET_ALL)
     elif choice == "2":
+
         #lie route
         print('"My uniform got shrunk in the laundry" you say')
         time.sleep(regular_delay)
+        print('"Alright. Pay more attention next time"')
+        time.sleep(regular_delay)
+        print("The guard walks away, then slips on a banana peel, then gets up and continues walking away.")
+        time.sleep(regular_delay)
+        print("You continue to wander through the boat, until eventually you find yourself in the bowels of the ship.")
+        time.sleep(regular_delay)
+        print('Eventually, you reach a door labeled "DO NOT ENTER"')
+        time.sleep(regular_delay)
+        print("1. Go through the door")
+        print("2. Look elsewhere")
+        choice = input("What do you do?\n")
+        if choice == "1":
 
+            #enter the door route
+            print("You open the door and go through, realizing there is nothing beneath your feet.")
+            time.sleep(regular_delay)
+            print('"That door just goes out the side of the boat!" You realize, as you plummet into the ocean')
+            time.sleep(regular_delay)
+            print("Due to a very deceptive door, you return home empty handed. Who does something like that anyway???")
+        elif choice == "2":
+
+            #go somewhere else route
+            print("You decide it's better to heed the door's warning and look elsewhere.")
+            time.sleep(regular_delay)
+            print("As you continue to traverse the ship, you see a sign in the corridor.")
+            time.sleep(regular_delay)
+            print('"NO UNAUTHORIZED ACCESS", this is a good sign. You continue in that direction.')
+            time.sleep(regular_delay)
+            print("Suddenly, you hear an approaching conversation. People are heading in your direction!")
+            time.sleep(regular_delay)
+            print("1. Try to hide")
+            print("2. Appear like you're supposed to be there")
+            choice = input("What do you do?\n")
+            if choice == "1":
+
+                #hide attempt route
+                print("You look around frantically, searching for a spot to hide, but there's no good spots!")
+                time.sleep(regular_delay)
+                print("The guards catch you in an unauthorized area. You try to fight back, but the guards have plot armor.")
+                time.sleep(regular_delay)
+                print("They overpower you, and bring you to the monstrous Mr.Lamb himself.")
+                time.sleep(regular_delay)
+                print('"I\'ve been expecting you" he says, as he turns around in his revolving chair.')
+                time.sleep(regular_delay)
+                print('"You were so close, and yet, so far."')
+                time.sleep(regular_delay)
+                print('"This is where it ends. No more games, no more choices."')
+                time.sleep(regular_delay)
+                print("A guard walks up to Mr.Lamb, and whispers something. Mr.Lamb's face twists into an expression of surprise.")
+                time.sleep(regular_delay)
+                print('"I\'ll deal with you later. Right now, I have more pressing concerns." Mr.Lamb gets up, and walks out of the room.')
+                time.sleep(regular_delay)
+                print("You have fallen into the hands of the villainous villain, Mr.Lamb, but his attention was drawn away by something else. What could've happened?")
+            elif choice == "2":
+
+                #nonchalant dreadhead route
+                print("You walk as if you're supposed to be there, ignoring the guards and continuing on your way.")
+                time.sleep(regular_delay)
+                print("One of them reaches out to block your path.")
+                time.sleep(regular_delay)
+                print('Hey, can\'t you read? Nobody is allowed here without permission!')
+                time.sleep(regular_delay)
+                print("At the risk of your pants spontaneously combusting, you lie again.")
+                time.sleep(regular_delay)
+                print('"I have permission to be here, directly from Mr.Lamb himself!" you reply sternly.')
+                time.sleep(regular_delay)
+                print("The man begins to shake at the mere mention of Mr.Lamb's name. He backs off.")
+                time.sleep(regular_delay)
+                print('"A- Alright. Sorry sir."')
+                time.sleep(regular_delay)
+                print("The guards slink out of sight, leaving you free to continue on your way.")
+                time.sleep(regular_delay)
+                print("You reach what appears to be a laboratory of some sort, a room made for designing nefarious devices.")
+                time.sleep(regular_delay)
+                print("The room is filled with a strange assortment of items. Saws and centrifuges, 3D printers, even random pieces of metal scrap.")
+                time.sleep(long_delay)
+                print("In the middle of the room, there is a small workbench with a stereotypical manila folder on it, labeled 'PLANS' in red ink.")
+                time.sleep(regular_delay)
+                print("Something valuable definitely has to be in here!")
+                time.sleep(regular_delay)
+                print("You open the folder, only to find that it is completely empty, save for a small sheet of paper.")
+                time.sleep(regular_delay)
+                print(Fore.GREEN + "Clara was here" + Style.RESET_ALL)
+                time.sleep(long_delay)
+                print("It appears the plans you were looking for were taken, but by whom?")
+                time.sleep(long_delay)
+                print("You have no choice but to return home, although, not empty handed.")
+                time.sleep(long_delay)
+                print("This mysterious new adversary, " + Fore.GREEN + "Clara " + Style.RESET_ALL + "will not be disappearing any time soon.")
 else:
     print("Error, please try again.")
     choice = input("Please enter '1' or '2'.\n")
